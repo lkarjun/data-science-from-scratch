3f6cf61da0af90eb7e1f780306820970dca41b72
lkarjun <lk.arjun@hotmail.com>
2021-01-09 19:19:33 +0530

    Updates: Forward_pass ( )

--- DeepNeuralNetwork.py
+++ DeepNeuralNetwork.py
@@ -10,6 +10,9 @@ class DeepNeuralNetwork:
         self.X = X
         self.Y = Y
         self.hidden_layers: List[int] = hidden_layers
+        self.param = None
+        self.Layer = None
+        self.cache = None
 
     def initialize_parameters(self):
         Layers = self.layer_size()
@@ -19,6 +22,7 @@ class DeepNeuralNetwork:
             parameters["W"+str(i)] = np.random.randn(Layers[i], Layers[i-1]) * 0.1
             parameters["b"+str(i)] = np.zeros(shape=(Layers[i], 1))
 
+        self.param = parameters
         return parameters
 
     def layer_size(self):
@@ -26,11 +30,26 @@ class DeepNeuralNetwork:
         Layer_last = [self.Y.shape[0]]
         Layer_hidden = self.hidden_layers
         full_layer = (Layer_0 + Layer_hidden) + Layer_last
-
+        self.Layer = full_layer
         return full_layer
 
     def forward_pass(self):
-        ...
+        cache: Dict[np.array] = {}
+        cache['Z1'] = (np.dot(self.param['W1'], 
+                        self.X)) + self.param['b1']
+        cache['A1'] = np.tanh(cache['Z1'])
+
+        print('=' * 30)
+        print(cache)
+        print('=' * 30)
+        for i in range(2, len(self.Layer)):
+            cache['Z'+str(i)] = (np.dot(self.param['W'+str(i)],
+                                    cache['A'+str(i-1)])) + self.param['b'+str(i)]
+            cache['A'+str(i)] = np.tanh(cache['Z'+str(i)])
+
+        self.cache = cache
+        return cache
+
     
     def backward_pass(self):
         ...
@@ -43,12 +62,17 @@ class DeepNeuralNetwork:
     
     def predict(self):
         ...
+    
+    def sigmoid(self, z):
+        return 1/(1+np.exp(-z))
 
 
-sample_data = np.array([[1,2,3],[2,3,4]]).reshape(2,-1)
-sample_x = np.array([[0, 1]]).reshape(1, -1)
-model = DeepNeuralNetwork(sample_data, sample_x, [2,64, 64, 2])
+sample_data = np.array([[1,2,3],[2, 6, 8]]).reshape(2,-1)
+sample_y = np.array([[0, 1]]).reshape(1, -1)
+model = DeepNeuralNetwork(sample_data, sample_y, [4, 10,20, 10, 3])
 
 print(model.layer_size())
 from pprint import pprint
 pprint(model.initialize_parameters())
+
+pprint(model.forward_pass())
